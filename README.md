ffeaa2503399aadd563f9092da7c0c13cfec73b1
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-26 12:42:24 +0200

    Add @rainbow-me/rainbowkit ethers wagmi

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # share-your-seat
+
+- Live: https://share-your-seat.vercel.app/
+
+## Stack
+
+- Typescript
+- NexJS
+- Tailwind & DaisyUI
+- Rainbowkit
+- EthersJS
+- Wagmi
+- Vercel
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,35 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import type { AppProps } from 'next/app';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { polygon, polygonMumbai } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [polygon, polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID || '' }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Share Your Seat',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
