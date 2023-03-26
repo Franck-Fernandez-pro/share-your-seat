eb8147580d9dd13e9a5bf3fc7d86afeb4469e557
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-26 12:16:31 +0200

    Update <head>

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="fr" data-theme="light">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://cdn.jsdelivr.net/npm/daisyui@2.51.5/dist/full.css"
           rel="stylesheet"
