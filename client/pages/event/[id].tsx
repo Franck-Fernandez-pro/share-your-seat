5d5a9e5deda1373a8fffa43462588925a54fe60d
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-26 19:48:16 +0200

    Add missing <Head>

--- client/pages/event/[id].tsx
+++ client/pages/event/[id].tsx
@@ -1,10 +1,19 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 const Event = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <p>Event: {id}</p>;
+  return (
+    <>
+      <Head>
+        <title>Événement {id}</title>
+      </Head>
+
+      <p>Event: {id}</p>
+    </>
+  );
 };
 
 export default Event;
