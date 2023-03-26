0b03daf1ca19e2ab1a3b5227e11c7c9b393ec1d8
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-26 12:14:06 +0200

    Add Tailwind and DaisyUI

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="fr">
-      <Head />
+    <Html lang="fr" data-theme="light">
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/daisyui@2.51.5/dist/full.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <script src="https://cdn.tailwindcss.com" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
