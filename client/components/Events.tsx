8d4215a8dba6f75c6573574144be794201b23b31
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-26 18:54:50 +0200

    Create <Events />

---
+++ client/components/Events.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import EventCard from './EventCard';
+
+const EVENTS = [
+  {
+    title: 'Event',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    id: 1,
+  },
+  {
+    title: 'Event',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    id: 2,
+  },
+  {
+    title: 'Event',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    id: 3,
+  },
+  {
+    title: 'Event',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    id: 4,
+  },
+  {
+    title: 'Event',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    id: 5,
+  },
+];
+
+interface Props {}
+
+const Events: FC<Props> = ({}) => (
+  <section id="events">
+    <h2 className="mx-auto my-20 text-center text-4xl font-extrabold md:text-6xl">
+      Événements
+    </h2>
+    <div className="flex flex-wrap justify-center gap-5">
+      {EVENTS.map(({ text, title, id }, idx) => (
+        <EventCard key={idx} text={text} title={title} href={`event/${id}`} />
+      ))}
+    </div>
+  </section>
+);
+
+export default Events;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,33 +1,6 @@
-import EventCard from '@/components/EventCard';
+import Events from '@/components/Events';
+import Intro from '@/components/Intro';
 import Head from 'next/head';
-
-const EVENTS = [
-  {
-    title: 'Event',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    id: 1,
-  },
-  {
-    title: 'Event',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    id: 2,
-  },
-  {
-    title: 'Event',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    id: 3,
-  },
-  {
-    title: 'Event',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    id: 4,
-  },
-  {
-    title: 'Event',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    id: 5,
-  },
-];
 
 export default function Home() {
   return (
@@ -38,16 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className="">
-        <section className="flex flex-wrap justify-center gap-5">
-          {EVENTS.map(({ text, title, id }, idx) => (
-            <EventCard
-              key={idx}
-              text={text}
-              title={title}
-              href={`event/${id}`}
-            />
-          ))}
-        </section>
+        <Intro />
+        <Events />
       </main>
     </>
   );
