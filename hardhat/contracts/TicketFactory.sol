cef0a1e50c55f109795af76455069dc8f6e78a56
Franck Fernandez <franck.fernandez@gmail.com>
2023-04-01 16:50:33 +0200

    Add TicketCreated and TicketMinted events + mintTicket function in factory

--- hardhat/contracts/TicketFactory.sol
+++ hardhat/contracts/TicketFactory.sol
@@ -19,8 +19,16 @@ contract TicketFactory {
     mapping(uint256 collectionIndex => address ownerAddress)
         public indexToOwner;
 
-    // Emitted when ERC-1155 token is deployed
-    event TicketCreated(address owner, address tokenContract);
+    // Emitted when ERC-1155 collection is deployed
+    event TicketCreated(address owner, address tokenContract, string name);
+
+    // Emmited when ERC-1155 collection is minted
+    event TicketMinted(
+        address owner,
+        address tokenContract,
+        string name,
+        uint256 amount
+    );
 
     /// Deploys a ERC-1155 token with given parameters - returns deployed address
     /// @param _eventName Name of ERC-1155 collection
@@ -42,8 +50,23 @@ contract TicketFactory {
         tokens.push(t);
         indexToContract[tokens.length - 1] = address(t);
         indexToOwner[tokens.length - 1] = tx.origin;
-        emit TicketCreated(msg.sender, address(t));
+
+        emit TicketCreated(msg.sender, address(t), t.name());
 
         return address(t);
+    }
+
+    /// Mint ERC-1155 token with given parameters
+    /// @param _index Index position in tokens array. Represents which ERC-1155 you want to interact with
+    /// @param _id Id being minted
+    /// @param _amount Amount of tokens you wish to mint
+    function mintTicket(uint _index, uint256 _id, uint256 _amount) public {
+        tokens[_index].mint(msg.sender, _id, _amount);
+        emit TicketMinted(
+            msg.sender,
+            address(tokens[_index]),
+            tokens[_index].name(),
+            _amount
+        );
     }
 }
