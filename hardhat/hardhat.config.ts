7a7454786ab8abfb085a5eb24af1c46ce4760ec5
Franck Fernandez <franck.fernandez@gmail.com>
2023-04-04 22:47:20 +0200

    Remove unused import

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,6 +1,5 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-import 'solidity-coverage';
 require('dotenv').config();
 
 const config: HardhatUserConfig = {
