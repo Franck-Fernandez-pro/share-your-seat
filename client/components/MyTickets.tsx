c5bf3a8831f249d94f109dbe32a48d6de8a36b86
Franck Fernandez <franck.fernandez@gmail.com>
2023-04-07 20:46:11 +0200

    Create <CollectionSection />

--- client/components/MyTickets.tsx
+++ client/components/MyTickets.tsx
@@ -1,32 +1,116 @@
 import { TicketFactoryContext } from '@/contexts/TicketFactory';
-import { FC, useContext, useEffect } from 'react';
-import { AiOutlineReload } from 'react-icons/ai';
+import { FC, useContext, useEffect, useMemo, useState } from 'react';
 import { useAccount } from 'wagmi';
+import MyTicketCard from './MyTicketCard';
+import { useCollection } from '@/hooks';
+import { ethers } from 'ethers';
+import { AiOutlineReload } from 'react-icons/ai';
 
 interface Props {}
 
 const MyTickets: FC<Props> = ({}) => {
   const { address } = useAccount();
-  const {
-    collections,
-    handler: { fetchCollections },
-  } = useContext(TicketFactoryContext);
+  const { collections } = useContext(TicketFactoryContext);
 
   return address ? (
     <section id="tickets">
-      <h2 className="mx-auto my-20 text-center text-5xl font-extrabold">
-        Tickets
-        <button className="ml-5" onClick={fetchCollections}>
-          <AiOutlineReload className="h-5 w-5" />
-        </button>
-      </h2>
-      <div className="flex flex-wrap justify-center gap-5">
-        {collections && collections.map((c, idx) => <p key={idx}>{c}</p>)}
+      <div className="">
+        {collections &&
+          collections.map((addr, idx) => (
+            <CollectionSection key={idx} addr={addr} />
+          ))}
       </div>
     </section>
   ) : (
     <p>Connectez vous</p>
   );
 };
-
 export default MyTickets;
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// :::::::::::::::::: CollectionSection ::::::::::::::::::::::
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+const CollectionSection: FC<{ addr: string }> = ({ addr }) => {
+  const [ids, setIds] = useState<number[]>([]);
+  const {
+    handler: { fetchCollection },
+  } = useContext(TicketFactoryContext);
+  const { ticketsLength, name, balanceOfBatch } = useCollection(addr, { ids });
+  const [collection, setCollection] = useState({
+    eventName: '',
+    uri: '',
+    availableTicketsLength: 0,
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, [addr]);
+
+  useEffect(() => {
+    if (ticketsLength === 0) return;
+    const newIds = [];
+    for (let i = 0; i < ticketsLength; i++) {
+      newIds.push(i);
+    }
+
+    setIds([...newIds]);
+  }, [ticketsLength]);
+
+  async function fetchData() {
+    const response = await fetchCollection(addr);
+
+    try {
+      const len = ethers.BigNumber.from(
+        response?.availableTicketsLength
+      ).toNumber();
+      if (response.eventName && response.uri && len) {
+        setCollection({
+          eventName: response.eventName,
+          uri: response.uri,
+          availableTicketsLength: len,
+        });
+      }
+    } catch (error) {}
+  }
+
+  function shouldRender(arr: number[]): boolean {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > 0) return true;
+    }
+    return false;
+  }
+
+  return shouldRender(balanceOfBatch) ? (
+    <>
+      <div>
+        <h2 className="mx-auto text-center text-2xl font-extrabold">
+          {name && name}
+          <button
+            className="ml-5"
+            onClick={() => {
+              fetchData();
+            }}
+          >
+            <AiOutlineReload className="h-5 w-5" />
+          </button>
+        </h2>
+        <div className="flex flex-wrap justify-center gap-5">
+          {balanceOfBatch &&
+            balanceOfBatch.map((b, i) => (
+              <MyTicketCard
+                key={i}
+                uri={`${collection.uri}${i}.json`}
+                id={`${i}`}
+                addr={addr}
+                balance={b}
+              />
+            ))}
+        </div>
+      </div>
+
+      <div className="divider" />
+    </>
+  ) : (
+    <></>
+  );
+};
