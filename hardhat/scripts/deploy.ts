13848adc4aaddf569738b8729c9b787cf9d83c88
Franck Fernandez <franck.fernandez@gmail.com>
2023-04-09 19:07:12 +0200

    Add Marketplace contract

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -4,8 +4,12 @@ async function main() {
   const TicketFactory = await ethers.getContractFactory('TicketFactory');
   const ticketFactory = await TicketFactory.deploy();
   await ticketFactory.deployed();
+  const Marketplace = await ethers.getContractFactory('Marketplace');
+  const marketplace = await Marketplace.deploy();
+  await marketplace.deployed();
 
   console.log(`ticketFactory deployed to ${ticketFactory.address}`);
+  console.log(`marketplace deployed to ${marketplace.address}`);
 }
 
 main().catch((error) => {
